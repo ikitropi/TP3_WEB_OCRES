2e18e82750e91b38cf0b11121d3a0c4ef443aadc
Inès Belloir <inesbelloir@Air-de-Ines.home>
2020-10-13 11:44:48 +0200

    question 1 2 3

--- js/API_Weather.js
+++ js/API_Weather.js
@@ -7,6 +7,8 @@ const API_URL = "https://api.openweathermap.org/data/2.5/weather";
 // Base source icon
 const API_URL_ICON = "http://openweathermap.org/img/wn/";
 
+const API_URL_DAILY = "https://api.openweathermap.org/data/2.5/forecast/daily";
+
 
 class API_WEATHER{
   constructor(city){
@@ -25,8 +27,19 @@ class API_WEATHER{
       crossdomain: true
     })
   }
+  getThreeDaysForecast(){
+  return axios
+    .get(`${API_URL_DAILY}?q=${this.city}&cnt=4&units=metric&appid=${API_KEY}`, {
+      crossdomain: true
+    })
+
+}
   // Retourne l'element HTML de l'icon symbolisant la méteo.
   getHTMLElementFromIcon(icon){
     return `<img src=${API_URL_ICON}${icon}@2x.png class="weather-icon"/>`
   }
 }
+
+
+
+
